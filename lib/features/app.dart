1b9dc7eb79280b7bdc8d531a0e9f00aa9d553867
anand <anand4t1@gmail.com>
2025-02-06 13:21:33 +0530

    added basic theme with font

--- lib/features/app.dart
+++ lib/features/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/core/theme.dart';
 import 'package:weather_app/features/home/presentation/bloc/home.bloc.dart';
 import 'package:weather_app/features/home/presentation/screens/home.screen.dart';
 import 'package:weather_app/features/setup.dart';
@@ -10,6 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: themeData,
       home: BlocProvider(
         create: (context) => HomeBloc(locator.get()),
         child: HomeScreen(),
