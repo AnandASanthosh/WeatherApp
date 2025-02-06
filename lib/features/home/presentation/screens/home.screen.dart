809ccc7d81b20cfee9fcf81d1af48c4a297b92a1
anand <anand4t1@gmail.com>
2025-02-06 16:39:27 +0530

    added network checking

--- lib/features/home/presentation/screens/home.screen.dart
+++ lib/features/home/presentation/screens/home.screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 import 'package:weather_app/features/home/presentation/widgets/error.card.dart';
 import 'package:weather_app/features/home/presentation/widgets/loading.indicator.dart';
@@ -20,20 +21,27 @@ class HomeScreen extends StatelessWidget {
           homeBloc.add(GetWeatherDataEvent(cityName));
         },
       ),
-      body: BlocBuilder<HomeBloc, HomeState>(
-        builder: (context, state) {
-          if (state is HomeLoading) {
-            return LoadingIndicator();
-          }
-          if (state is HomeError) {
-            return SingleChildScrollView(child: ErrorCard(message: state.message));
-          }
-          if (state is HomeLoaded) {
-            return SingleChildScrollView(child: WeatherCard(data: state.data));
-          }
-          return Center(child: Text("Use the above text field to search for a city"));
-        },
-      ),
+      body: StreamBuilder<Object>(
+          stream: Connectivity().onConnectivityChanged,
+          builder: (context, snapshot) {
+            if (snapshot.data == ConnectivityResult.none) {
+              return SingleChildScrollView(child: ErrorCard(message: "No internet connection"));
+            }
+            return BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                if (state is HomeLoading) {
+                  return LoadingIndicator();
+                }
+                if (state is HomeError) {
+                  return SingleChildScrollView(child: ErrorCard(message: state.message));
+                }
+                if (state is HomeLoaded) {
+                  return SingleChildScrollView(child: WeatherCard(data: state.data));
+                }
+                return Center(child: Text("Use the above text field to search for a city"));
+              },
+            );
+          }),
     );
   }
 }
